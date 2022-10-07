7a4a00001571c10b409b088927b8c47b9ab04200
QuocTA-Amela <101373375+QuocTA-Amela@users.noreply.github.com>
2022-10-07 08:54:59 +0700

    fix def self run

--- lib/fastlane/plugin/installonair/actions/installonair_action.rb
+++ lib/fastlane/plugin/installonair/actions/installonair_action.rb
@@ -16,53 +16,53 @@ module Fastlane
 
       def self.run(params)
         UI.message("The installonair plugin is working!")
+        Actions.verify_gem!('rest-client')
+        require 'rest-client'
+        require 'json'
+  
+        if options[:file].nil?
+          UI.important("File didn't come to install_on_air_plugin. Uploading is unavailable.")
+          return
+        end
+        if options[:token].nil?
+          UI.important("Install on air token is nil - uploading is unavailable.")
+          UI.important("Try to upload file by yourself. Path: #{options[:file]}")
+          return
+        end
+  
+        upload_options[:_token] = options[:token]
+        upload_options[:file] = File.new(options[:file], 'rb')
+        UI.success("Start uploading file to Install On Air. Please, be patient. This could take some time.")
+        response = RestClient.post(UPLOAD_URL, upload_options)
+  
+        begin
+          response
+        rescue RestClient::ExceptionWithResponse => error
+            UI.important("Faild to upload file to diawi, because of:")
+            UI.important(error)
+            UI.important("Try to upload file by yourself. Path: #{options[:file]}")
+            return
+        end
+  
+        job = JSON.parse(response.body)['job']
+        
+        if job
+            timeout = options[:timeout].clamp(5, 1800)
+            check_status_delay = options[:check_status_delay].clamp(1, 30)
+  
+            if check_status_delay > timeout
+                UI.important("`check_status_delay` is greater than `timeout`")
+            end
+  
+            UI.success("Upload completed successfully.")
+        end  
       end
+      
       def self.description
         "Install On Air plugin integrated with Fastlane"
       end
       def self.authors
         ["QuocTA-Amela"]
-      end
-
-      Actions.verify_gem!('rest-client')
-      require 'rest-client'
-      require 'json'
-
-      if options[:file].nil?
-        UI.important("File didn't come to install_on_air_plugin. Uploading is unavailable.")
-        return
-      end
-      if options[:token].nil?
-        UI.important("Install on air token is nil - uploading is unavailable.")
-        UI.important("Try to upload file by yourself. Path: #{options[:file]}")
-        return
-      end
-
-      upload_options[:_token] = options[:token]
-      upload_options[:file] = File.new(options[:file], 'rb')
-      UI.success("Start uploading file to Install On Air. Please, be patient. This could take some time.")
-      response = RestClient.post(UPLOAD_URL, upload_options)
-
-      begin
-        response
-      rescue RestClient::ExceptionWithResponse => error
-          UI.important("Faild to upload file to diawi, because of:")
-          UI.important(error)
-          UI.important("Try to upload file by yourself. Path: #{options[:file]}")
-          return
-      end
-
-      job = JSON.parse(response.body)['job']
-      
-      if job
-          timeout = options[:timeout].clamp(5, 1800)
-          check_status_delay = options[:check_status_delay].clamp(1, 30)
-
-          if check_status_delay > timeout
-              UI.important("`check_status_delay` is greater than `timeout`")
-          end
-
-          UI.success("Upload completed successfully.")
       end
 
       def self.return_value
